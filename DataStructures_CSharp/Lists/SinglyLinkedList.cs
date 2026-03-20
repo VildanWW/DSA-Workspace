1b732e425f312d97ad88b889f683e010de788cbf
VildanWW <akbulatov.vildan@gmail.com>
2026-03-20 06:45:17 +0500

    Добавлены методы Insert, RemoveLast, RemoveFirst, Clear. FindElement. Так же добавлен индексатор и интерфейс IEnumerable.

--- DataStructures_CSharp/Lists/SinglyLinkedList.cs
+++ DataStructures_CSharp/Lists/SinglyLinkedList.cs
@@ -4,14 +4,100 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DataStructures_CSharp.Lists {
-    internal class SinglyLinkedList<T> {
+    internal class SinglyLinkedList<T> : IEnumerable<T> {
         public Node<T> head { get; private set; }
         public Node<T> tail { get; private set; }
         public int count { get; private set; }
 
         public SinglyLinkedList() { count = 0; }
+
+        public IEnumerator<T> GetEnumerator() {
+            Node<T> current = head;
+            while(current !=null) {
+                yield return current.value;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+
+        public T this[int index] {
+            
+            get {
+                return FindElement(index);
+            }
+
+            set {
+                if (index < 0 || index >= count) throw new IndexOutOfRangeException();
+                Node<T> current = head;
+                for(int i=0;i<index;i++) {
+                    current = current.next;
+                }
+                current.value = value;
+            }
+        }
+
+        public T FindElement(int index) {
+            Node<T> current = head;
+
+            if (index < 0 || index >= count) throw new IndexOutOfRangeException();
+
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+
+            return current.value;
+        }
+
+        public void Clear() {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void RemoveFirst() {
+            Remove(0);
+        }
+
+        public void RemoveLast() {
+            Remove(count - 1);
+        }
+
+        public void Insert(int index, T value) {
+            if(index<0 || index>count) throw new IndexOutOfRangeException();
+
+            Node<T> current = head;
+
+            if(index==0) {
+                Node<T> node = new Node<T>(value);
+
+                node.next = current;
+                head = node;
+                if (count == 0) tail = head;
+            }
+            else {
+                
+                for(int i=0;i<index-1;i++) {
+                    current = current.next;
+                }
+
+                Node<T> node = new Node<T>(value);
+                node.next = current.next;
+                current.next = node;
+
+                if(node.next == null) {
+                    tail = node;
+                }
+            }
+
+            count++;
+        }
 
         public void Add(T value) {
             if (head == null) {
@@ -28,41 +114,29 @@ namespace DataStructures_CSharp.Lists {
             count++;
         }
 
-        public T FindElement(int index) {
-            Node<T> current = head;
-
-            if (index < 0 || index >= count) throw new IndexOutOfRangeException();
-
-            for(int i=0;i<index;i++) {
-                current = current.next;
-            }
-
-            return current.value;
-        } 
-
         public void Remove(int index) {
-            if (index < 0 || index >= count) throw new IndexOutOfRangeException();
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
 
             Node<T> current = head;
 
-            if(index==0) {
+            if (index == 0) {
                 head = head.next;
+                if (count == 1)
+                    tail = null;
             }
-            
-            else if(index!=count-1) {
-                for(int i=0;i<index-1;i++) {
-                    current = current.next;
-                }
-
-                Node<T> cur = current.next.next;
-
-                current.next = cur;
-            } else {
-                while(current.next.next!=null) {
+            else if (index == count - 1) {
+                while (current.next != tail) {
                     current = current.next;
                 }
                 tail = current;
                 current.next = null;
+            }
+            else {
+                for (int i = 0; i < index - 1; i++) {
+                    current = current.next;
+                }
+                current.next = current.next.next;
             }
 
             count--;
--- DataStructures_CSharp/Program.cs
+++ DataStructures_CSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructures_CSharp.Lists;
 using System;
+using System.Linq;
 
 namespace DataStructures_CSharp {
     class Program {
@@ -7,14 +8,13 @@ namespace DataStructures_CSharp {
             SinglyLinkedList<int> singlyLinkedList = new SinglyLinkedList<int>();
 
             singlyLinkedList.Add(2);
-            singlyLinkedList.Add(1);
-            singlyLinkedList.Add(42);
-
-            singlyLinkedList.Remove(2);
+            singlyLinkedList.Add(23);
+            singlyLinkedList.Insert(1, 22);
 
             singlyLinkedList.ShowList();
 
-            Console.WriteLine(singlyLinkedList.tail.value);
+            Console.WriteLine(singlyLinkedList.count);
+            
         }
     }
 }
