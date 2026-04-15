d8b04cb03ab5caebf2f32f921dd41f8f25cf6c03
VildanWW <akbulatov.vildan@gmail.com>
2026-04-15 17:57:53 +0500

    Создана хеш-таблица с некоторыми базовыми методами и пару хеш-функций.

--- DataStructures_CSharp/HashTable/HashTable.cs
+++ DataStructures_CSharp/HashTable/HashTable.cs
@@ -1,23 +1,24 @@
 ﻿
+using System.Collections;
+
 namespace DataStructures_CSharp.HashTable {
-    public class HashTable<KeyType, ValueType> {
+    public class HashTable<KeyType, ValueType> : IEnumerable<KeyValuePair<KeyType, ValueType>>  {
         private const int DefaultCapacity = 16;
         private const double DefaultLoadFactor = 0.75;
-        
+
         private int _hashSize;
         private Func<KeyType, int, int> _hashFunction;
-        private LinkedList<KeyValuePair<KeyType, ValueType>>[] _hashMassive;
+        private Node<KeyType, ValueType>[] _hashMassive;
         private int _countInsert;
+
+        public int HashSize => _hashSize;
+        public int CountInsert => _countInsert;
 
         public HashTable() {
             _hashSize = DefaultCapacity;
             _countInsert = 0;
 
-            _hashMassive = new LinkedList<KeyValuePair<KeyType, ValueType>>[DefaultCapacity];
-
-            for (int i = 0; i < DefaultCapacity; i++) {
-                _hashMassive[i] = new LinkedList<KeyValuePair<KeyType, ValueType>>();
-            }
+            _hashMassive = new Node<KeyType, ValueType>[DefaultCapacity];
 
             _hashFunction = (key, size) => HashFunctions.Fnv1aHash(key, size);
         }
@@ -28,20 +29,16 @@ namespace DataStructures_CSharp.HashTable {
             _hashFunction = hashFunction;
             _countInsert = 0;
 
-            _hashMassive = new LinkedList<KeyValuePair<KeyType, ValueType>>[hashSize];
-
-            for (int i = 0; i < hashSize; i++) {
-                _hashMassive[i] = new LinkedList<KeyValuePair<KeyType, ValueType>>();
-            }
+            _hashMassive = new Node<KeyType, ValueType>[hashSize];
         }
 
         public bool TryUpdate(KeyType key, ValueType value) {
             int index = _hashFunction(key, _hashSize);
-            var current = _hashMassive[index].First;
+            var current = _hashMassive[index];
 
             while(current != null) {
-                if(current.Value.Key.Equals(key)) {
-                    current.Value = new KeyValuePair<KeyType, ValueType>(key, value);
+                if(EqualityComparer<KeyType>.Default.Equals(current.Key, key)) {
+                    current.Value = value;
                     return true;
                 }
                 current = current.Next;
@@ -51,42 +48,123 @@ namespace DataStructures_CSharp.HashTable {
         }
 
         public void ReHash() {
-            if(GetLoadFactor() >= 0.75) {
-                var hashMassiveNew = new LinkedList<KeyValuePair<KeyType, ValueType>>[_hashSize * 2];
 
-                for(int i=0;i<hashMassiveNew.Length;i++) {
-                    hashMassiveNew[i] = new LinkedList<KeyValuePair<KeyType, ValueType>>();
-                }
+            int newCapacity = _hashSize * 2;
+            var newBuckets = new Node<KeyType, ValueType>[newCapacity];
 
-                for(int i=0;i<_hashSize;i++) {
-                    var current = _hashMassive[i].First;
-                    while(current!=null) {
-                        int newIndex = _hashFunction(current.Value.Key, _hashSize * 2);
-                        hashMassiveNew[newIndex].AddLast(current.Value);
-                        current = current.Next;
-                    }
+            for(int i=0;i<_hashSize;i++) {
+                var current = _hashMassive[i];
+                while(current!=null) {
+                    int newIndex = _hashFunction(current.Key, newCapacity);
+                    
+                    var nextInOldList = current.Next;
+
+                    current.Next = newBuckets[newIndex];
+                    newBuckets[newIndex] = current;
+
+                    current = nextInOldList;
                 }
-                _hashMassive = hashMassiveNew;
-                _hashSize = _hashSize * 2;
             }
+            _hashMassive = newBuckets;
+            _hashSize = _hashSize * 2;
         }
 
         public void Add(KeyType key, ValueType value) {
-            if (!TryUpdate(key, value)) {
-                int index = _hashFunction(key, _hashSize);
-                _countInsert++;
-                _hashMassive[index].AddLast(new KeyValuePair<KeyType, ValueType>(key, value));
+            if (TryUpdate(key, value)) {
+                return;
+            }
 
+            if(GetLoadFactor()>=DefaultLoadFactor) {
                 ReHash();
             }
+
+            int index = _hashFunction(key, _hashSize);
+            _countInsert++;
+            var newNode = new Node<KeyType, ValueType>(key, value);
+            newNode.Next = _hashMassive[index];
+            _hashMassive[index] = newNode;
         }
 
         public double GetLoadFactor() {
-            int countFilledPlace = 0;
-            for(int i=0;i<_hashSize;i++) {
-                countFilledPlace += _hashMassive[i].Count;
+            return (double)_countInsert / _hashSize;
+        }
+
+        public bool Find(KeyType key) {
+            int index = _hashFunction(key, _hashSize);
+            var current = _hashMassive[index];
+
+            while (current != null) {
+                if (EqualityComparer<KeyType>.Default.Equals(current.Key, key)) {
+                    return true;
+                }
+                current = current.Next;
             }
-            return (double)countFilledPlace / _hashSize;
+            return false;
+        }
+
+        public int GetEmptyBuckets() {
+            int count = 0;
+            for (int i = 0; i < _hashSize; i++) {
+                if (_hashMassive[i] == null) count++;
+            }
+            return count;
+        }
+
+        public IEnumerator<KeyValuePair<KeyType, ValueType>> GetEnumerator() {
+            for(int i=0;i<_hashSize;i++) {
+                var current = _hashMassive[i];
+
+                while(current!=null) {
+                    yield return new KeyValuePair<KeyType, ValueType>(current.Key, current.Value);
+                    current = current.Next;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        public void Remove(KeyType key) {
+            int index = _hashFunction(key, _hashSize);
+            var current = _hashMassive[index];
+
+            if (current == null) return;
+
+            if (EqualityComparer<KeyType>.Default.Equals(current.Key, key)) {
+                _hashMassive[index] = current.Next;
+                _countInsert--;
+                return;
+            }
+
+            while (current.Next != null) {
+                if (EqualityComparer<KeyType>.Default.Equals(current.Next.Key, key)) {
+                    current.Next = current.Next.Next;
+                    _countInsert--;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public void ShowTable() {
+            for (int i = 0; i < _hashSize; i++) {
+                Console.Write($"{i}: ");
+                var current = _hashMassive[i];
+
+                while (current != null) {
+                    Console.Write($"[Key:{current.Key}, Val:{current.Value}]");
+                    current = current.Next;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Clear() {
+            for(int i=0;i<_hashSize;i++) {
+                _hashMassive[i] = null;
+            }
+            _countInsert = 0;
         }
     }
 }
---
+++ DataStructures_CSharp/HashTable/Node.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace DataStructures_CSharp.HashTable {
+    public class Node<KeyType, ValueType> {
+        public KeyType Key { get; set; }
+        public ValueType Value { get; set; }
+        public Node<KeyType, ValueType> Next { get; set; }
+
+        public Node(KeyType key, ValueType value) {
+            Key = key;
+            Value = value;
+        }
+    }
+}
